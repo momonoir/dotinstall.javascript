42fb8264d50ea0435504d8030977f0a3a5e7d058
momonoir <momoko.hattori.17@gmail.com>
2017-08-30 17:26:59 +0900

    おみくじ #8

--- omikuzi/js/main.js
+++ omikuzi/js/main.js
@@ -4,9 +4,14 @@
   var btn = document.getElementById('btn');
 
   btn.addEventListener('click', function () {
-    var n = Math.floor(Math.random() * 3);
+    //配列を使う
+    var results = ['大吉', '小吉', '凶', '末吉'];
+    //var n = Math.floor(Math.random() * 3);
+    var n = Math.floor(Math.random() * results.length); //配列の要素数だけ掛ける
+    this.textContent = results[n];
 
 /*
+switch文の場合
     switch (n) {
       case 0:
         this.textContent = '大吉';
@@ -18,15 +23,17 @@
           this.textContent = '凶';
           break;
     }
-if (n === 0) {
-  this.textContent = '大吉';
-} else if (n === 1) {
-  this.textContent = '小吉';
-} else {
-  this.textContent = '凶';
-}
-*/
- //this.textContent = n;
+
+if文の場合
+  if (n === 0) {
+    this.textContent = '大吉';
+  } else if (n === 1) {
+    this.textContent = '小吉';
+  } else {
+    this.textContent = '凶';
+  }
+  */
+   //this.textContent = n;
   });
   btn.addEventListener('mousedown', function () {
     this.className = 'pushed';
