7cf7b4bcce26dee6fc32976de601725a41d0d68b
momonoir <momoko.hattori.17@gmail.com>
2017-08-30 17:13:23 +0900

    おみくじ #1-#7
    おみくじ結果をif文で出す場合と、switch文で出す場合を作成

---
+++ omikuzi/js/main.js
@@ -0,0 +1,38 @@
+(function () {
+  'use strict';
+
+  var btn = document.getElementById('btn');
+
+  btn.addEventListener('click', function () {
+    var n = Math.floor(Math.random() * 3);
+
+/*
+    switch (n) {
+      case 0:
+        this.textContent = '大吉';
+        break;
+      case 1:
+        this.textContent = '小吉';
+        break;
+        case 2:
+          this.textContent = '凶';
+          break;
+    }
+if (n === 0) {
+  this.textContent = '大吉';
+} else if (n === 1) {
+  this.textContent = '小吉';
+} else {
+  this.textContent = '凶';
+}
+*/
+ //this.textContent = n;
+  });
+  btn.addEventListener('mousedown', function () {
+    this.className = 'pushed';
+  });
+  btn.addEventListener('mouseup', function () {
+    this.className = '';
+  });
+
+}) ();
